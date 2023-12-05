e5327b251e0a08416d3d50b11a196c828d7f675c
VQuocDinh <vqdinh2202@gmail.com>
2023-12-05 15:51:58 +0700

    update

--- index.js
+++ index.js
@@ -9,11 +9,16 @@ btnMenu.onclick = function () {
     } else {
         header.style.height = null
     }
-}   
+}
 
 var menuItems = document.querySelectorAll('#nav li a[href*="#"]')
 for (var i = 0; i < menuItems.length; i++) {
-        menuItems[i].onclick = function () {
+    menuItems[i].onclick = function (e) {
+        var isCloseMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav')
+        if (isCloseMenu) {
+            e.preventDefault();
+        } else {
             header.style.height = null
         }
     }
+}
